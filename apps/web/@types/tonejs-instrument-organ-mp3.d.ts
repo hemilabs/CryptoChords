82ce96060f8e6e2ac0a379256eedc041b65745d3
rafappelt <rafael@appelt.com.br>
2024-03-19 11:34:48 -0300

    add instruments
    Signed-off-by: rafappelt <rafael@appelt.com.br>

--- apps/web/@types/tonejs-instrument-organ-mp3.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'tonejs-instrument-organ-mp3' {
-  import { Sampler } from 'tone'
-  export default class InstrumentOrganMp3 extends Sampler {
-    constructor(options?: {
-      minify?: boolean
-      onload?: unknown
-    })
-  }
-}
--- apps/web/@types/tonejs-instrument-saxophone-mp3.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'tonejs-instrument-saxophone-mp3' {
-  import { Sampler } from 'tone'
-  export default class InstrumentSaxophoneMp3 extends Sampler {
-    constructor(options?: {
-      minify?: boolean
-      onload?: unknown
-    })
-  }
-}
