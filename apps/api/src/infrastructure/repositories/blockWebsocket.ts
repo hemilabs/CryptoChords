6de6f30ecc3ee12297a594506a71b7d23bbaa7f3
rafappelt <rafael@appelt.com.br>
2025-02-26 10:02:19 -0300

    improve WebSocket connection handling and add keep-alive mechanism
    Signed-off-by: rafappelt <rafael@appelt.com.br>

--- apps/api/src/infrastructure/repositories/blockWebsocket.ts
+++ apps/api/src/infrastructure/repositories/blockWebsocket.ts
@@ -1,4 +1,4 @@
-import Web3, { BlockHeaderOutput } from 'web3';
+import Web3, { BlockHeaderOutput, WebSocketProvider } from 'web3';
 import { EventEmitter } from 'events';
 import { TxType } from '@cryptochords/shared';
 import { TxTypesEnum } from '@cryptochords/shared';
@@ -12,12 +12,80 @@ export class BlockWebsocketRepository
   implements BlockRepository
 {
   private web3: Web3 | null = null;
+  private provider: WebSocketProvider | null = null;
+  private websocketUrl!: string;
+  private noDataTolerance!: number;
+  private lastBlockTimestamp: number = 0;
+  private connectionTimestamp: number = 0;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private subscription: any | null = null;
 
-  execute(websocketUrl: string): void {
-    this.web3 = new Web3(websocketUrl);
+  execute(websocketUrl: string, noDataTolerance: number = 30_000): void {
+    this.websocketUrl = websocketUrl;
+    this.noDataTolerance = noDataTolerance;
+    this.connect();
+  }
+
+  connect(): void {
+    console.log('Trying to connect..');
+    this.web3 = new Web3(this.getProvider());
+  }
+
+  keepAlive(): void {
+    const intervalId = setInterval(() => {
+      if (this.connectionTimestamp == 0) {
+        return;
+      }
+
+      const referenceTimestamp = Math.max(
+        this.connectionTimestamp,
+        this.lastBlockTimestamp,
+      );
+
+      const timeSinceLastData = Date.now() - referenceTimestamp;
+
+      if (timeSinceLastData > this.noDataTolerance) {
+        console.log('No data received for too long, reconnecting');
+        clearInterval(intervalId);
+        this.reconnect();
+        return;
+      }
+    }, this.noDataTolerance / 4);
+  }
+
+  getProvider() {
+    if (this.provider) {
+      return this.provider;
+    }
+
+    this.provider = new WebSocketProvider(this.websocketUrl);
+    this.provider.once('error', this.handleConnectionError.bind(this));
+    this.provider.once('end', this.handleConnectionEnd.bind(this));
+    this.provider.once('connect', this.handleConnection.bind(this));
+    return this.provider;
+  }
+
+  handleConnectionError(): void {
+    console.log('Connection error');
+    this.reconnect();
+  }
+
+  handleConnectionEnd(): void {
+    console.log('Connection ended');
+    this.reconnect();
+  }
+
+  handleConnection(): void {
+    console.log('Connected to websocket');
+    this.connectionTimestamp = Date.now();
     this.subscribeToNewBlockHeaders();
+    this.keepAlive();
+  }
+
+  reconnect(delay: number = 10_000): void {
+    console.log('Reconnecting in', delay, 'ms');
+    this.stop();
+    setTimeout(() => this.connect(), delay);
   }
 
   stop(): void {
@@ -25,6 +93,20 @@ export class BlockWebsocketRepository
       this.subscription.unsubscribe();
       this.subscription = null;
     }
+
+    if (this.provider) {
+      this.provider.removeAllListeners('connect');
+      this.provider.removeAllListeners('error');
+      this.provider.removeAllListeners('end');
+      this.provider = null;
+    }
+
+    if (this.web3) {
+      this.web3.removeAllListeners();
+      this.web3 = null;
+    }
+
+    this.connectionTimestamp = 0;
   }
 
   private async subscribeToNewBlockHeaders(): Promise<void> {
@@ -32,7 +114,7 @@ export class BlockWebsocketRepository
       return;
     }
 
-    this.subscription = await this.web3?.eth.subscribe('newBlockHeaders');
+    this.subscription = await this.web3.eth.subscribe('newBlockHeaders');
     this.subscription.on('data', async (blockHeader: BlockHeaderOutput) => {
       await this.handleNewBlockHeader(this.web3, blockHeader);
     });
@@ -42,6 +124,7 @@ export class BlockWebsocketRepository
     web3: Web3 | null,
     blockHeader: BlockHeaderOutput,
   ): Promise<void> {
+    this.lastBlockTimestamp = Date.now();
     this.emit(
       TxTypesEnum.Block,
       L2Block.create({
