f9daed01e190c9da2f871d006edbf85aaf427b43
arturdolzan <arturdolzan@gmail.com>
2025-04-10 09:03:51 -0300

    Fix repository links and apply hemi-socials

--- apps/web/src/presentation/react/components/NavItems.tsx
+++ apps/web/src/presentation/react/components/NavItems.tsx
@@ -1,11 +1,14 @@
 import { NavItem } from './NavItem';
+import { githubUrl } from 'hemi-socials';
 
 export const NavItems = function (props: { className?: string }) {
   return (
     <nav className={`flex flex-row gap-8 ${props.className ?? ''}`}>
-      <NavItem href={import.meta.env.VITE_GITHUB_URL}>Github</NavItem>
-      <NavItem href={import.meta.env.VITE_CONTRIBUTORS_URL}>Contribute</NavItem>
-      <NavItem href={import.meta.env.VITE_FEEDBACK_URL}>Feedback</NavItem>
+      <NavItem href={`${githubUrl}/CryptoChords`}>Github</NavItem>
+      <NavItem href={`${githubUrl}/CryptoChords/blob/master/CONTRIBUTING.md`}>
+        Contribute
+      </NavItem>
+      <NavItem href={`${githubUrl}/CryptoChords/issues/new`}>Feedback</NavItem>
     </nav>
   );
 };
